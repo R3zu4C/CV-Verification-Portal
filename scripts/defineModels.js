9acb3695d0bd6b70ac9d0fdf7d5a9935456936cd
R3zu4C <a.patanwal@protonmail.com>
2021-12-12 18:06:43 +0530

    fixed typo

--- scripts/defineModels.js
+++ scripts/defineModels.js
@@ -2,15 +2,15 @@
 
 const defineModels = async () => {
   await require('../models/User');
+  await require('../models/Organization');
   await require('../models/Admin');
   await require('../models/Point');
   await require('../models/Flag');
-  await require('../models/Group');
-  await require('../models/GroupToPerm');
-  await require('../models/Organization');
+  await require('../models/Role');
   await require('../models/Permission');
   await require('../models/Notification');
-  await require('../models/AdminToGroup');
+  await require('../models/AdminToRole');
+  await require('../models/RoleToPerm');
   await require('../models/Request');
 
   console.log("All tables created successfully");
