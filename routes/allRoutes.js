f9caaf43f0f0770691461bd13d76239e206a052b
Aman Kumar <79211216+akr-25@users.noreply.github.com>
2021-12-21 01:54:23 +0530

    logout added

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -6,10 +6,22 @@ const { searchPoint } = require("../controllers/Search");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
+  
   res.end(fs.readFileSync("./views/index.html"));
 });
 // Adding a Point thru POST request
 router.post("/point", addPoint);
+
+router.get("/status", (req, res) => {
+  let user = req.session.user
+  let admin = req.session.admin;
+  let loggedIn = req.session.user ? true : false;
+
+  if(loggedIn)
+  res.send({ user, admin });
+  else
+  res.json("NA");
+})
 
 // Getting a list of all the Organizations and their org_id
 router.get("/orgs", fetchAllOrg);
