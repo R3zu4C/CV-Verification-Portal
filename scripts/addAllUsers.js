1996dc1b6e4030032eadd014c1e102575bca1a29
Aman Kumar <79211216+akr-25@users.noreply.github.com>
2021-12-22 20:02:01 +0530

    addAllUser script corrected

--- scripts/addAllUsers.js
+++ scripts/addAllUsers.js
@@ -2,7 +2,7 @@
 
 const fastcsv = require("fast-csv");
 const fs = require("fs");
-const User = require("../models/User");
+const {User }= require("../models");
 const sequelize = require("../database/connection");
 
 const addAllUsers = async () => {
