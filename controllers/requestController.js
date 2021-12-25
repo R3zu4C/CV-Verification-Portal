37d23e3a7f0b612e937c0dd6bc8cd01045b48324
R3zu4C <a.patanwal@protonmail.com>
2021-12-25 23:52:56 +0530

    approving request updates flag approval

--- controllers/requestController.js
+++ controllers/requestController.js
@@ -15,10 +15,16 @@ module.exports = {
     const request = await Request.findByPk(reqId);
     const point = await request.getPoint();
     const requests = await point.getRequests();
+    const flags = await point.getFlags({ where: { approved_by: null }});
 
     for (const _req of requests) {
       await _req.update({ approved: 1 });
     }
+
+    for (const flag of flags) {
+      await flag.update({ approved_by: approvedBy });
+    }
+    
     await point.update({ approved_by: approvedBy });
 
     res.send({ redirect: "/" });
