ef47460c205cf75533f369f34c420d2a43eeb795
Aman Kumar <79211216+akr-25@users.noreply.github.com>
2022-01-13 01:20:15 +0530

    added some org routes

--- controllers/orgController.js
+++ controllers/orgController.js
@@ -5,4 +5,83 @@ module.exports = {
     const orgs = await Organization.findAll();
     res.send(orgs);
   },
+
+  createOrg: async (req, res) => {
+    try {
+      const { org_id, name, parent_id } = req.body;
+      const org = await Organization.create({ org_id, name });
+
+      if (parent_id) {
+        await org.setOrganization(parent_id);
+      }
+
+      res.send(org);
+    }
+    catch (err) {
+      console.log(err);
+      res.send(err);
+    }
+  },
+
+  getOrgWithChildren: async (req, res) => {
+    try {
+      const { org_id } = req.params;
+      const org = await Organization.findByPk(org_id, {
+        include: [
+            Organization,
+        ],
+      });
+      res.send(org);
+    }
+    catch (err) {
+      console.log(err);
+      res.send(err);
+    }
+  },
+
+  getOrgsWithChildren: async (req, res) => {
+    try {
+      const orgs = await Organization.findAll({
+        include: [
+          Organization,
+        ],
+      });
+      res.send(orgs);
+    }
+    catch (err) {
+      console.error(err);
+      res.send(err);
+    }
+  },
+
+  updateOrg: async (req, res) => {
+    try {
+      const { org_id, parent_id, name } = req.body;
+      const org = await Organization.findByPk(org_id);
+
+      if (name) await org.update({ name });
+
+      if (parent_id) await org.setOrganization(parent_id);
+      res.send(org);
+    }
+    catch (err) {
+      console.error(err);
+      res.send(err);
+    }
+  },
+
+  deleteOrg: async (req, res) => {
+    try {
+      const { org_id } = req.body;
+      const org = await Organization.findByPk(org_id);
+      await org.destroy();
+      res.send(org);
+    }
+    catch (err) {
+      console.error(err);
+      res.send(err);
+    }
+  },
+
+
 };
--- routes/orgRoutes.js
+++ routes/orgRoutes.js
@@ -1,9 +1,22 @@
-const { fetchAllOrg } = require("../controllers/orgController");
-
+const { fetchAllOrg , createOrg, getOrgWithChildren, getOrgsWithChildren, updateOrg, deleteOrg} = require("../controllers/orgController");
+const {requireAuth} = require("../middleware/authMiddleware");
 const router = require("express").Router();
 
 
 // Getting a list of all the Organizations and their org_id
 router.get("/", fetchAllOrg);
+
+
+//? work in progess, dont use these routes
+router.post("/create", requireAuth, createOrg);
+
+router.get("/all", getOrgsWithChildren); //! error: Organization table is not unique
+
+router.post("/update",requireAuth, updateOrg);
+
+router.delete("/delete", requireAuth, deleteOrg);
+
+router.get("/:org_id", getOrgWithChildren); //! error: Organization table is not uniques
+
 
 module.exports = router;
