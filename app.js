9d143c255a32be2e515f042420496917e013a6b6
R3zu4C <a.patanwal@protonmail.com>
2022-01-13 21:09:24 +0530

    can get the proof by link

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 require("dotenv").config();
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
 const passport = require("passport");
 const passportSetup = require("./passport");
 
@@ -43,7 +44,7 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
-// app.use(express.static("public"));
+app.use(express.static(path.resolve('./public')));
 
 // Routes
 app.use("/", homeRoute);
