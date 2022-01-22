f5dc3bbc67954d14fab03f49e2ec3b3394612c60
Aman Kumar <79211216+akr-25@users.noreply.github.com>
2022-01-23 01:47:57 +0530

    request controller minor change

--- controllers/requestController.js
+++ controllers/requestController.js
@@ -1,4 +1,4 @@
-const { Admin, Request , sequelize } = require("../models");
+const { Admin, Request, sequelize } = require("../models");
 const AdminService = require("./helpers/adminHelper")
 
 module.exports = {
@@ -9,10 +9,10 @@ module.exports = {
     res.send(requests);
   },
 
-  approveRequest: async (req, res) => {
+  respondToRequest: async (req, res, status) => {
     const transactionID = await sequelize.transaction();
     try {
-      const approvedBy = req.session.user.user_id;
+      const responseBy = req.session.user.user_id;
       const reqId = req.params.reqId;
 
       const request = await Request.findByPk(reqId);
@@ -20,16 +20,19 @@ module.exports = {
 
       const adminService = new AdminService(req.session.user, req.session.admin);
       if (adminService.hasPermission("Approve requests", point.org_id) === false)
-        return res.status(403).send({ error: "You do not have permission to approve this request" });
+        return res.status(403).send({ error: "You do not have permission to respond to this request" });
 
       const requests = await point.getRequests();
       const flags = await point.getFlags({ where: { response_by: null } });
 
-      await Promise.all(requests.map(_req => _req.update({ approved: 'A' }, { transaction: transactionID })));
+      await Promise.all(requests.map(_req => _req.update({ approved: status }, { transaction: transactionID })));
 
-      await Promise.all(flags.map(flag => flag.update({ response_by: approvedBy, status: "A" }, { transaction: transactionID })));
+      await Promise.all(flags.map(flag => flag.update({ response_by: responseBy, status: status }, { transaction: transactionID })));
 
-      await point.update({ response_by: approvedBy, status: 'A' }, { transaction: transactionID });
+      await point.update({ response_by: responseBy, status: status }, { transaction: transactionID });
+
+      if (req.body.remark)
+        await request.update({ remark: req.body.remark }, { transaction: transactionID });
 
       transactionID.commit();
 
@@ -38,43 +41,7 @@ module.exports = {
       console.error("Error:" + error.message);
 
       transactionID.rollback();
-      
-      res.status(500).send({ error, redirect: "/" });
-    }
-  },
 
-  rejectRequest: async (req, res) => {
-    const transactionID = await sequelize.transaction();
-    try {
-      const rejectedBy = req.session.user.user_id;
-      const reqId = req.params.reqId;
-
-      const request = await Request.findByPk(reqId);
-      const point = await request.getPoint();
-
-      const adminService = new AdminService(req.session.user, req.session.admin);
-      if (adminService.hasPermission("Approve requests", point.org_id) === false)
-        return res.status(403).send({ error: "You do not have permission to approve this request" });
-
-      const requests = await point.getRequests();
-      const flags = await point.getFlags({ where: { response_by: null } });
-
-      await Promise.all(requests.map(_req => _req.update({ approved: 'D' }, { transaction: transactionID })));
-
-      await Promise.all(flags.map(flag => flag.update({ response_by: rejectedBy, status: "A" }, { transaction: transactionID })));
-
-      await point.update({ response_by: rejectedBy, status: 'D' }, { transaction: transactionID });
-
-      await request.update({ remark: req.body.remark }, { transaction: transactionID });
-
-      transactionID.commit();
-
-      res.send({ redirect: "/" });
-    } catch (error) {
-      console.error("Error:" + error.message);
-
-      transactionID.rollback();
-      
       res.status(500).send({ error, redirect: "/" });
     }
   },
