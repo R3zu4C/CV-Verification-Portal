a7e4ca4d41033f0f65013a602579e4b08f93087b
Aman Kumar <79211216+akr-25@users.noreply.github.com>
2022-02-12 16:50:31 +0530

    pendingFlags added to api

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,5 +1,7 @@
 const { Point, Admin, User, Flag, sequelize } = require("../models");
 const fs = require("fs");
+const AdminService = require("../controllers/helpers/adminHelper");
+const { Op } = require("sequelize");
 
 const router = require("express").Router();
 
@@ -8,36 +10,54 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/api", async (req, res) => {
-  const allPoints = await Point.findAll({
-    where: {
-      visibility: "P",
-    }
-  });
+  try {
+    const allPoints = await Point.findAll({
+      where: {
+        visibility: "P",
+      }
+    });
 
-  const user_id = req.session.user.user_id;
+    const user_id = req.session.user.user_id;
 
-  const user = await User.findByPk(user_id, { include: ["Flags", "Points"] });
-  
-  const admin = await Admin.findOne({ where: { admin_id: user_id } });
-  const requests = await admin.getRequests({ include: "Point" });
+    const user = await User.findByPk(user_id, { include: ["Flags", "Points"] });
 
-  const subQuery = `SELECT point_id FROM points WHERE user_id = '${req.session.user.user_id}'`;
-  const flagsOfUsers = await Flag.findAll({
-    where: {
-      point_id: [sequelize.literal(subQuery)],
-    },
-    include: ["Point"],
-  });
+    const admin = await Admin.findOne({ where: { admin_id: user_id } });
+    const requests = await admin.getRequests({ include: "Point" });
 
-  const flagsOfAdmins = await Flag.findAll({
-    where: {
-      response_by: user_id,
-    },
-    include: ["Point"],
-  })
+    const subQuery = `SELECT point_id FROM points WHERE user_id = '${req.session.user.user_id}'`;
+    const flagsOfUsers = await Flag.findAll({
+      where: {
+        point_id: [sequelize.literal(subQuery)],
+      },
+      include: ["Point"],
+    });
 
-  res.send({ user, allPoints, requests, flagsOfUsers, flagsOfAdmins });
+    const respondedFlagsOfAdmin = await Flag.findAll({
+      where: {
+        response_by: user_id,
+      },
+      include: ["Point"],
+    })
 
+    const adminService = new AdminService(req.session.user, req.session.admin);
+    let adminOrg = adminService.org
+    adminOrg = Object.keys(adminOrg).map(org => parseInt(org))
+    adminOrg = adminOrg.filter(org => org !== 0)
+    const orgSubQuery = `SELECT point_id FROM points WHERE org_id IN (${adminOrg.join(",")})`;
+    console.log(orgSubQuery)
+    const pendingFlagsOfAdmin = await Flag.findAll({
+      where: {
+        point_id: [sequelize.literal(orgSubQuery)],
+        response_by: { [Op.eq]: null }
+      },
+      include: ["Point"],
+    })
+    return res.send({ user, allPoints, requests, flagsOfUsers, respondedFlagsOfAdmin, pendingFlagsOfAdmin });
+  } catch (err) {
+    res.status(500).send({
+      err,
+    });
+  }
 })
 
 module.exports = router;
